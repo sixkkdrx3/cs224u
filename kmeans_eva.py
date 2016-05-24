e438d04de322ca8099d237f3d18a92efaa23de67
Kaidi Yan <kaidi@stanford.edu>
2016-05-23 20:30:59 -0700

    added greedy assignment as described in milestone

--- kmeans_eva.py
+++ kmeans_eva.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
         actual_bins[int(line[0])] = actual_bins[int(line[0])] + 1
 
     for i in range(13):
-        print actual_bins[i]
+        print actual_bins[i],
+    print
     print '=' * 79
 
 
@@ -40,6 +41,43 @@ if __name__ == "__main__":
 
     print total_cnt / (cnt * 1.0)
 
-            
+    print '=' * 79
+    C = range(13)
+    S = range(13)
+    greedy_assignment = dict()
+    while len(C) > 0:
+        best_cnt = -1
+        best_cluster = 0
+        best_category = 0
+        for c in C:
+            max_cnt = -1
+            max_cluster = 0
+            for s in S:
+                cnt = 0
+                cluster = clusters[s]
+                for cc in cluster:
+                    if actual_clusters[cc] == c:
+                        cnt = cnt + 1
+                if cnt > max_cnt:
+                    max_cnt = cnt
+                    max_cluster = s
+
+            if max_cnt > best_cnt:
+                best_cnt = max_cnt
+                best_cluster = max_cluster
+                best_category = c
+
+        C.remove(best_category)
+        S.remove(best_cluster)
+        greedy_assignment[best_cluster] = best_category
 
 
+    print greedy_assignment
+    total_cnt = 0
+    for i in range(13):
+        cluster = clusters[i]
+        for c in cluster:
+            if actual_clusters[c] == greedy_assignment[i]:
+                total_cnt = total_cnt + 1
+
+    print total_cnt / (40000.0)
